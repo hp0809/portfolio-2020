af80a18a4e4b9f513356451a9f5e99cf774d1671
Hali Power <hpower0809@gmail.com>
2020-10-05 09:43:26 -0230

    Updates

---
+++ precache-manifest.c8f074e73c6df39630949db9647f01d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2797450dc56e8ba5009dd21e6a4dad6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3a83afd88017ae6663d8",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f29ece731f21b2276a11",
+    "url": "/portfolio/static/js/2.e4e08dad.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/portfolio/static/js/2.e4e08dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a83afd88017ae6663d8",
+    "url": "/portfolio/static/js/main.dd9f608c.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
